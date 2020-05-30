e0fa951780641e51bdc3a493d5d0338e66c50426
ramyasusarla <rsusarla06@gmail.com>
2020-05-30 14:57:59 -0400

    updated projects

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -2,6 +2,8 @@ $(function() {
 	smoothScroll(500);
 	projectTrans();
 	projectLoad();
+
+	$("header h1").fitText(1, { minFontSize: '20px', maxFontSize: '72px' });
 });
 
 function smoothScroll (duration) {
@@ -45,4 +47,37 @@ function projectLoad(){
 		$('.title').text(newTitle);
 	});
 }
+
+(function( $ ){
+
+  $.fn.fitText = function( kompressor, options ) {
+
+    // Setup options
+    var compressor = kompressor || 1,
+        settings = $.extend({
+          'minFontSize' : Number.NEGATIVE_INFINITY,
+          'maxFontSize' : Number.POSITIVE_INFINITY
+        }, options);
+
+    return this.each(function(){
+
+      // Store the object
+      var $this = $(this);
+
+      // Resizer() resizes items based on the object width divided by the compressor * 10
+      var resizer = function () {
+        $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
+      };
+
+      // Call once to set.
+      resizer();
+
+      // Call on resize. Opera debounces their resize by default.
+      $(window).on('resize.fittext orientationchange.fittext', resizer);
+
+    });
+
+  };
+
+})( jQuery );
 
